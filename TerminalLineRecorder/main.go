e53f7b54360d9162c0ab3f0f726f8574a90d06a7
mudabbir-ahmad <mudabbira.uk@gmail.com>
2026-07-17 02:03:39 +0100

    sumn light

--- TerminalLineRecorder/main.go
+++ TerminalLineRecorder/main.go
@@ -1,3 +1,49 @@
 package main
 
-func main() {}
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/term"
+)
+
+func main() {
+	// Create a reader
+	reader := bufio.NewReader(os.Stdin)
+
+	//Create a file path:
+	err := os.Mkdir("./Log", os.ModePerm)
+
+	check(err)
+
+	path := filepath.Join("./Log", "TerminalLog.txt")
+	file, _ := os.Create(path) // Creating the file and assinging it the variable file
+
+	// Create a writer
+	writer := bufio.NewWriter(file)
+
+	// The rest.
+	fmt.Println("Enter in commands you were to run in the terminal, this go file acts as a middle man recorder: \n")
+	input, _ := reader.ReadString('.')
+
+	var inputs []string
+
+	cleanedInput := strings.Split(inputs, "\n")
+
+	writer.WriteString(cleanedInput)
+
+	term.NewTerminal(_, input) * Terminal
+
+	defer writer.Flush()
+	defer file.Close()
+
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,9 +2,6 @@ module LearningGo
 
 go 1.26.5
 
-require (
-	github.com/howeyc/gopass v0.0.0-20210920133722-c8aef6fb66ef // indirect
-	golang.org/x/crypto v0.54.0 // indirect
-	golang.org/x/sys v0.47.0 // indirect
-	golang.org/x/term v0.45.0 // indirect
-)
+require golang.org/x/term v0.45.0
+
+require golang.org/x/sys v0.47.0 // indirect
